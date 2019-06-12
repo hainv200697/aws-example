3ca257c1f3cafd0db0b33a19cd1ea823508d443c
hainv97 <hainv200697@gmail.com>
2019-06-12 11:18:09 +0700

    edit messsage

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     @GetMapping
     public String demo() {
-        return "Demo Cloud Computing";
+        return "Demo CICD AWS";
     }
 }
